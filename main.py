19ccc794ec3724ab86e1b88cbeededdb5a71395f
alanvn <alanvan3791@gmail.com>
2022-12-26 00:21:43 -0300

    try and exception catch working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 
 def format_cpf(CPF):
-    return CPF.replace('.','').replace('-','')[0:9]
+    return CPF.replace('.','').replace('-','')[:-2]
 
 def product_sum(cpf_digitless):
     product = 0 
@@ -25,22 +25,25 @@ def add_two_digits(cpf_digitless):
     return cpf_digitless
 
 def validate_cpf():
-    CPF = input("Enter the CPF to be checked (optional dots ands the hyphen): ")
-    if len(CPF) != 11 and len(CPF) != 14:
-        raise ValueError
+    try:
+        CPF = input("Enter the CPF to be checked (optional dots ands the hyphen): ")
+        if len(CPF) != 11 and len(CPF) != 14:
+            raise ValueError
 
-    cpf_digitless = format_cpf(CPF)
+        cpf_digitless = format_cpf(CPF)
 
-    final_cpf = add_two_digits(cpf_digitless)
-    final_cpf = f'{final_cpf[0:3]}.{final_cpf[3:6]}.{final_cpf[6:9]}-{final_cpf[9:11]}'
+        if len(cpf_digitless) != 9:
+            raise ValueError
 
-    if final_cpf.replace('.','').replace('-','') == CPF:
-        print(f"The entered CPF is valid! (matches the calculated one - {final_cpf})")
-    else:
-        print(f"The entered CPF is invalid. (it does not match the calculated one - {final_cpf})")
+        final_cpf = add_two_digits(cpf_digitless)
+        final_cpf = f'{final_cpf[0:3]}.{final_cpf[3:6]}.{final_cpf[6:9]}-{final_cpf[9:11]}'
 
-try:
-    validate_cpf()
-except ValueError:
-    print("\n\nEnter a format-valid CPF - XXX.XXX.XXX-XX (11 numbers)\n")
-    validate_cpf()
+        if final_cpf.replace('.','').replace('-','') == CPF:
+            print(f"The entered CPF is valid! (matches the calculated one - {final_cpf})")
+        else:
+            print(f"The entered CPF is invalid. (it does not match the calculated one - {final_cpf})")
+    except ValueError:
+        print("\n\nEnter a format-valid CPF - XXX.XXX.XXX-XX (11 numbers)\n")
+        validate_cpf()
+
+validate_cpf()
